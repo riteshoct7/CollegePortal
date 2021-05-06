b9e390579c144b7b98250b471055956466ab5be5
Ritesh Parekh <Riteshp@tblocks.com>
2021-05-06 20:04:38 +0530

    Added Dashboard Controller for Admin Module

---
+++ CollegePortal.Web/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CollegePortal.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class DashboardController : Controller
+    {
+
+        #region Constructors
+        public DashboardController()
+        {
+
+        }
+        #endregion
+
+        #region Methods
+
+        public IActionResult Dashboard()
+        {
+            return View();
+        } 
+
+        #endregion
+
+    }
+}
